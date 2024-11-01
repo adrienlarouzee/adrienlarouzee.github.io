09b634456cd7bcf7abdf5443baed36e6567b6b43
adrienlarouzee <adrien.larouzee@gmail.com>
2024-11-01 13:12:00 +0100

    Init Player

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ document.addEventListener("DOMContentLoaded", function () {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 });
+
+let player;
+
+// Fonction appelée par l'API YouTube une fois chargée
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player("youtube-player", {
+        height: "100%",
+        width: "100%",
+        videoId: "dQw4w9WgXcQ", // Remplace cette ID par celle de n’importe quelle vidéo pour tester
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
